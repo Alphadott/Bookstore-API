2a5a4354da543dbc7bbab86685b477d23c08cdd8
Michael Smith <115336372+Bros-SO@users.noreply.github.com>
2022-10-16 04:50:02 +0100

    Updated the db
    Added more optimized way of connecting to the mongodb instance

--- db.js
+++ db.js
@@ -6,11 +6,13 @@ const MONGO_DB_CONNECTION_URI = process.env.MONGO_DB_CONNECTION_URI
 //function to connect to mongodb
 function connectToMongoDB(){
     mongoose.connect(MONGO_DB_CONNECTION_URI)
-
+    
+    
+    //mongodb connected
     mongoose.connection.on("connected", () => {
         console.log("Connected to MongoDB Successfully!")
     })
-
+    //mongodb not connected
     mongoose.connection.on("error", (err) => {
         console.log(err)
         console.log("An error occured while connecting to MongoDB!")
